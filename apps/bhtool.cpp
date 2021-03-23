ef561863b8ed57ff9b50662336ba61dfb7b77316
Butch Howard <butchhoward@gmail.com>
2021-03-22 22:13:23 -0400

    Create a function map. Call function is modified args

--- apps/bhtool.cpp
+++ apps/bhtool.cpp
@@ -1,9 +1,36 @@
 #include <bhtool/bhtool.hpp>
+#include <bhtool/stderrred.hpp>
 
 #include <fmt/format.h>
 
-int main() {
-    fmt::print("bhtool: {}\n",  "ready for code" );
+#include <iostream>
 
-    return 0;
+
+bhtool::commands command_map = {
+    {std::string(STDERRRED_CMD), bhtool::stderrred}
+};
+
+
+int main(int argc, char *argv[])
+{
+    for (auto i = 0; i < argc; ++i)
+    {
+        fmt::print("bhtool: {} - {}\n",  i, argv[i] );
+    }
+
+    if (argc < 2)
+    {
+        return 1;
+    }
+
+    auto cmd = command_map.find(std::string(argv[1]));
+    if (cmd == command_map.end())
+    {
+        return 2;
+    }
+
+    fmt::print("bhtool calling: {}\n",  cmd->first );
+    auto code = cmd->second(--argc, &(argv[1]));
+
+    return code;
 }
